7741e18120de7ac1f56c80a726f13b7d923b21ec
Dishant Shah <dishantshah3133@gmail.com>
2023-12-13 16:23:22 +0530

    Models

---
+++ server/models/User.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const UserS = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    gender:{
+        type:String,
+        required:true
+    },
+    income:[],
+    budget:[],
+    expense:[]
+},
+{
+    timestamps:true
+}
+)
+
+
+
+module.exports = mongoose.model('user',UserS)
