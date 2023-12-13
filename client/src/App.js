e349442afbbdb24d2659123420a47ad0abe48129
rosh0409 <roshan.2002kumr@gmail.com>
2023-12-13 18:25:58 +0530

    signup backend done

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Signup from './pages/Signup';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path={"/signup"}element={<Signup/>}/>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path={"/signup"} element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+      <Toaster />
+    </>
   );
 }
 
