592951c9201d0833a524ee3b5dff1cd1ba44c062
rosh0409 <roshan.2002kumr@gmail.com>
2023-12-13 16:32:06 +0530

    controller setup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 import express from "express";
 import "./db/Connection.js";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import { routes } from "./routes/userRoutes.js";
 
 const app = express();
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(cors({ credentials: true, origin: ["http://localhost:3000"] }));
 
 app.use("/api/user", routes);
 
