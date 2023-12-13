1ab9c6cd5524877212c29585062c9feb2732be46
rosh0409 <roshan.2002kumr@gmail.com>
2023-12-13 16:11:53 +0530

    db connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,9 @@
 import express from "express";
+import "./db/Connection.js";
 
 const app = express();
 
 app.listen(8000, (err) => {
   if (err) return err.message;
   console.log("server started");
-  console.log("dishant")
 });
-
-
